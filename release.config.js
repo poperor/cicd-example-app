407f00b473b77ff3bdced52c6eb5aaf2b8863c2e
Per Halvor Tryggeseth <per.halvor.tryggeseth@origo.oslo.kommune.no>
2021-02-11 14:22:48 +0100

    changed branch name from master to main everywhere

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: "master",
+  branches: "main",
   repositoryUrl: "https://github.com/poperor/cicd-example-app",
   plugins: [
     "@semantic-release/commit-analyzer",
