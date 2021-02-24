e11cbc6e52a7f494aed8ec242233452dceedd2a9
Per Halvor Tryggeseth <per.halvor.tryggeseth@origo.oslo.kommune.no>
2021-02-24 14:03:48 +0100

    fix: fix

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -61,7 +61,7 @@ try {
   const body = core.getInput("body");
   const assignees = core.getInput("assignees");
 
-  const octokit = new github.GitHub(token);
+  const octokit = new github.GitHub.octokit(token);
   const response = octokit.issues.create({
     owner: github.context.repo.owner,
     repo: github.context.repo,
