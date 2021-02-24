d9cb034d2e91b5f4a9de885fae1f63ea2b061992
Per Halvor Tryggeseth <per.halvor.tryggeseth@origo.oslo.kommune.no>
2021-02-24 14:32:02 +0100

    fix: fix
    BREAKING CHANGE: asdfasfd

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -62,7 +62,7 @@ async function run() {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    const octokit = new github.GitHub(token);
+    const octokit = new github.getOctokit(token);
     const response = await octokit.issues.create({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
