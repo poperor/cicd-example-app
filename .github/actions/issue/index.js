e704c24148d46126630b759f42d39073dcd19154
Per Halvor Tryggeseth <per.halvor.tryggeseth@origo.oslo.kommune.no>
2021-02-24 14:25:36 +0100

    fix: fix

--- .github/actions/issue/index.js
+++ .github/actions/issue/index.js
@@ -24,3 +24,28 @@ async function run() {
 }
 
 run();
+
+// async function run() {
+//   try {
+//     const token = core.getInput("token");
+//     const title = core.getInput("title");
+//     const body = core.getInput("body");
+//     const assignees = core.getInput("assignees");
+
+//     const octokit = new github.GitHub(token);
+
+
+//     const response = await octokit.issues.create({
+//       // owner: github.context.repo.owner,
+//       // repo: github.context.repo.repo,
+//       ...github.context.repo,
+//       title,
+//       body,
+//       assignees: assignees ? assignees.split("\n") : undefined
+//     });
+
+//     core.setOutput("issue", JSON.stringify(response.data));
+//   } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
