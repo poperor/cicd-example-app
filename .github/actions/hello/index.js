a2a710d52d9c35b5f0eee2b34871604fa98fb92b
Per Halvor Tryggeseth <per.halvor.tryggeseth@origo.oslo.kommune.no>
2021-02-24 11:13:00 +0100

    feat: better github action

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,15 +1,21 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-// throw(new Error("feila"));
+
 
 try {
+  // throw(new Error("feila"));
+  core.debug("Debug message by poperor")
+  core.warning("Warning message by poperor")
+  core.error("Error message by poperor")
   const name = core.getInput("who-to-greet");
-
+  core.setSecret(name);
   console.log(`Hallo ${name}`);
   const time = new Date();
   core.setOutput("time", time.toTimeString());
-
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+  core.exportVariable("POP_HELLO", "Hallo fra poperor");
 } catch (error) {
   core.setFailed("faila altså", error.message);
 }
