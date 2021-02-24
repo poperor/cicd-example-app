1ad4f8e101211197b4da0f62824aabe86a312960
Per Halvor Tryggeseth <per.halvor.tryggeseth@origo.oslo.kommune.no>
2021-02-24 13:54:25 +0100

    feat: new github action

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  const token = core.getInput("token");
+  const title = core.getInput("title");
+  const body = core.getInput("body");
+  const asignees = core.getInput("assignees");
+
+  const octokit = new github.GitHub(token);
+  const response = octokit.issues.create({
+    owner: github.context.repo.owner,
+    repo: github.context.repo,
+    title,
+    body,
+    asignees: assignees ? assignees.spli('\n') : undefined
+  });
+
+  core.setOutput('issue', JSON.stringify(response.data));
+  
+} catch (error) {
+  core.setFailed("faila altså", error.message);
+}
